50c84ab0a770fa325aa3fa6639db9bdb7b401cc6
Fabio <fabio@pop-os.localdomain>
2023-11-03 10:11:17 -0500

    Solution for problem 1 completed

--- divisibleBy3_5/main.cpp
+++ divisibleBy3_5/main.cpp
@@ -3,10 +3,25 @@
 *
 * Fabio Ventura
 * Divisible by 3 and 5 (Project Euler)
-* Purpose: Program finds the sum of 9 - 1000 of the numbers dvisibly by 3 and 5
+* Purpose: Program finds the sum of numbers from 0 - 1000  dvisibly by 3 and 5
 *
 *
 **************************/
 #include "main.h"
 
+int main(int argc, char** argv)
+{
+    int sum = 0;
+    for(int i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+
+}
 
