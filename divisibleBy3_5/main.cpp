8f175ab863cc73e9cf6316bf03f908d34775b29c
Fabio <fabio@pop-os.localdomain>
2023-11-03 08:50:44 -0500

    Updating folder

---
+++ divisibleBy3_5/main.cpp
@@ -0,0 +1,12 @@
+/*************************
+* 
+*
+* Fabio Ventura
+* Divisible by 3 and 5 (Project Euler)
+* Purpose: Program finds the sum of 9 - 1000 of the numbers dvisibly by 3 and 5
+*
+*
+**************************/
+#include "main.h"
+
+
---
+++ divisibleBy3_5/main.h
@@ -0,0 +1,17 @@
+/**********************
+ *
+ *  
+ * File: main.h
+ * Author: Fabio Ventura
+ *
+ *
+ **********************/
+
+
+#include <stdlib.h>   /* srand, rand */
+#include <time.h>     /* time */
+#include <iostream>   /* cout, endl */
+#include <string>     /* string */
+
+using std::cout;
+using std::endl;
